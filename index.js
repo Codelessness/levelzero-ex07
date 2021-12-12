92476a58f76836f4b774d8f9c65139699f8e01a2
Ashutosh Aman <ashutoshsingh161216@gmail.com>
2021-12-12 18:36:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// ex07: Function to add two numbers
+
+//Programming is all about (input >> processing >> output)
+function add(numberOne, numberTwo){
+
+console.log("numberOne is:  ",  numberOne, "numberTwo is ", numberTwo );
+
+// console.log("numberOne is:  " + numberOne, "numberTwo is " + numberTwo );
+
+var sum = numberOne+numberTwo;
+return sum;
+}
+ 
+
+var result = add(2,9);
+console.log("The sum of 2 and 9 is : " + result);
+
+
